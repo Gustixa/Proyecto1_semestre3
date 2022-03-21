4b5f3ade571044f2760f1dd1fddb911cd644c6ee
Samuel Argueta <arg211024@uvg.edu.gt>
2022-03-20 19:49:43 -0600

    Mejoras en la estructura

---
+++ src/AritmethicOperationResult.java
@@ -0,0 +1,22 @@
+
+/**
+ * @author MAAG
+ *
+ */
+public class AritmethicOperationResult implements IOperationResult {
+
+	String key;
+	String result;
+	
+	@Override
+	public void performOperation() {
+		System.out.println("El resultado de la operacion " + key + " es: " + result);
+	}
+
+	@Override
+	public void addResults(String key, String result) {
+		this.key = key;
+		this.result = result;
+	}
+
+}
---
+++ src/IOperationResult.java
@@ -0,0 +1,8 @@
+/**
+ * @author MAAG
+ *
+ */
+public interface IOperationResult {
+	public void performOperation();
+	public void addResults(String key, String result);
+}
